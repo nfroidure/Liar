380f2f599f1dbf8adca8921f007e0b3e19b59ef7
Nicolas Froidure <nicolas_froidure@elitwork.com>
2013-07-11 14:14:11 +0200

    Updated libs.sh

--- libs.sh
+++ libs.sh
@@ -7,13 +7,15 @@ wget -O http://requirejs.org/docs/release/2.1.6/comments/require.js > www/javasc
 
 # Commandor
 mkdir www/javascript/libs/commandor
-wget -O https://raw.github.com/nfroidure/Commandor/master/Sounds.js > www/javascript/libs/commandor/Commandor.js
+wget -O https://raw.github.com/nfroidure/Commandor/master/Commandor.js > www/javascript/libs/commandor/Commandor.js
+wget -O https://raw.github.com/nfroidure/Commandor/master/CommandPromise.js > www/javascript/libs/commandor/CommandPromise.js
 
 # Promise
 mkdir www/javascript/libs/promise
 wget -O https://raw.github.com/nfroidure/Promise/master/Promise.js > www/javascript/libs/promise/Promise.js
-wget -O https://raw.github.com/nfroidure/Promise/master/XHRPromise.js > www/javascript/libs/promise/XHRPromise.js
-wget -O https://raw.github.com/nfroidure/Promise/master/WebSocketPromise.js > www/javascript/libs/promise/WebSocketPromise.js
+mkdir www/javascript/libs/promise/dom
+wget -O https://raw.github.com/nfroidure/Promise/master/dom/XHRPromise.js > www/javascript/libs/promise/dom/XHRPromise.js
+wget -O https://raw.github.com/nfroidure/Promise/master/dom/WebSocketPromise.js > www/javascript/libs/promise/dom/WebSocketPromise.js
 
 # Sounds
 mkdir www/javascript/libs/sounds
